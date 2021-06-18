1bdc18847f034f78486633eaaf62b52331ffd119
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-06-18 09:12:49 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,27 @@
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    sprite.image.flipX()
+    sprite.vx = mySprite.vx * -1
+})
+let mySprite: Sprite = null
+tiles.setTilemap(tilemap`層級1`)
+mySprite = sprites.create(img`
+    . . . . . . f f f f f f . . . . 
+    . . . . f f e e e e f 2 f . . . 
+    . . . f f e e e e f 2 2 2 f . . 
+    . . . f e e e f f e e e e f . . 
+    . . . f f f f e e 2 2 2 2 e f . 
+    . . . f e 2 2 2 f f f f e 2 f . 
+    . . f f f f f f f e e e f f f . 
+    . . f f e 4 4 e b f 4 4 e e f . 
+    . . f e e 4 d 4 1 f d d e f . . 
+    . . . f e e e 4 d d d d f . . . 
+    . . . . f f e e 4 4 4 e f . . . 
+    . . . . . 4 d d e 2 2 2 f . . . 
+    . . . . . e d d e 2 2 2 f . . . 
+    . . . . . f e e f 4 5 5 f . . . 
+    . . . . . . f f f f f f . . . . 
+    . . . . . . . f f f . . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTilesByType(assets.tile`myTile`)[0])
+tiles.setTileAt(tiles.getTilesByType(assets.tile`myTile`)[0], assets.tile`myTile1`)
+mySprite.vx = 40
